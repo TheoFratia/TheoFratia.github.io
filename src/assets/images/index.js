ffcacb601861c4fab00a218d33486537529d5fc3
fratia.theo <theofratia22@gmail.com>
2023-11-30 16:43:26 +0100

    Test2DeployReactThreeJs

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import logo from './logo.svg'
+import andreCote from './andre-cote.png'
+import loire from './loire.png'
+
+export {
+    hero,
+    meta,
+    shopify,
+    logo,
+    andreCote, 
+    loire
+}
